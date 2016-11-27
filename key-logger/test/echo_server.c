c2e3dd2f2ab262f5ddcdaf55c7cf01fab1c6139b
Jun Young Park <pjy9605@gmail.com>
2016-11-27 20:24:36 +0900

    access only 127.0.0.1 ip address

--- key-logger/test/echo_server.c
+++ key-logger/test/echo_server.c
@@ -70,7 +70,7 @@ int main(int argc, char *argv[])
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(default_port);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if(bind(server_sock, (struct sockaddr* )&server_addr, sizeof(server_addr)) == -1)
     {
